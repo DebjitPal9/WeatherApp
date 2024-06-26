edefe3b63a9fea5484a30e0bd86bf5a64cd080ad
Diptoprovo <mondaldipto1606@gmail.com>
2024-06-26 18:13:11 +0530

    final commit on styles

--- Weather_App/src/Components/GeoCoding.jsx
+++ Weather_App/src/Components/GeoCoding.jsx
@@ -20,16 +20,16 @@ function GeoCoding(props) {
         setLoc(event.target.value);
     }
     return (
-        <div>
-            <div className="flex items-center justify-center">
-                <input type="text" id="first_name" className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 min-w-80 md:width-50 mr-0.5" value={loc} onChange={location} placeholder="Enter Country/City name..." required />
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 max-w-15" onClick={handleClick}>Search</button>
+        <div className="flex flex-col w-screen">
+            <div className="flex flex-row items-center justify-center">
+                <input type="text" id="first_name" className="w-5/12 bg-sky-200 border border-blue-500 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-800 block p-2.5 mr-0.5" value={loc} onChange={location} placeholder="Enter Country/City name..." required />
+                <button type="submit" className="max-w-20 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 max-w-15" onClick={handleClick}>Search</button>
             </div>
-            <div className="my-2">
-                { data[0]?  <h1 className="font-mono">Search Results(Click to view):</h1> : <></>
+            <div className="my-2 flex flex-col items-center justify-center">
+                { data[0]?  <h1 className="font-mono w-5/12">Search Results(Click to view):</h1> : <></>
                 // <h1 className="font-mono">Search Results(Click to view):</h1>
                 }
-                <ul >
+                <ul className=" w-1/3">
                     {data ? data.map((loc, index) => <li className ="max-w-lg w-fit" key={index}><div className="no-underline hover:underline hover:cursor-pointer" onClick={() => props.handler(loc.lat, loc.lon)}> {loc.name}, {loc.state}, {loc.country}</div> </li>) : <div>Loading ...</div>}
                 </ul>
             </div>
