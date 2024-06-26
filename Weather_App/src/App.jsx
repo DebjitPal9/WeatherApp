b84daf31c9823b5eb034de0b3bb73a17d1530492
Debjit Pal <63508896+DebjitPal9@users.noreply.github.com>
2024-06-26 11:21:03 +0530

    1st APi call

--- Weather_App/src/App.jsx
+++ Weather_App/src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import React,{ useState, useEffect} from 'react'
 import Manager from './Components/Manager'
 
 function App() {
+  const [data, setData] = useState();
+   useEffect(() => {
+      fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=ee22166bcb5ed367827f2147e80762a5')
+         .then((response) => response.json())
+         .then((data) => {
+            console.log(data);
+            setData(data);
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
+   }, []);
   return (
     <>
       <div>
-        <Manager></Manager>
+      {data ? <div>{JSON.stringify(data)}</div> : <div>Loading...</div>}
       </div>
     </>
   )
