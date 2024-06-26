bc2ff791ba1415ad9660dac4a0f5359303c32399
Debjit Pal <63508896+DebjitPal9@users.noreply.github.com>
2024-06-26 13:05:44 +0530

    Widget added

--- Weather_App/src/App.jsx
+++ Weather_App/src/App.jsx
@@ -1,27 +1,27 @@
-import React,{ useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Manager from './Components/Manager'
 import GeoCoding from './Components/GeoCoding'
 function App() {
-  const [data, setData] = useState();
-  //  useEffect(() => {
-  //     fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=ee22166bcb5ed367827f2147e80762a5')
-  //        .then((response) => response.json())
-  //        .then((data) => {
-  //           console.log(data);
-  //           setData(data);
-  //        })
-  //        .catch((err) => {
-  //           console.log(err.message);
-  //        });
-  //  }, []);
-  // return (
-  //   <>
-  //     <div>
-  //     {data ? <div>{JSON.stringify(data)}</div> : <div>Loading...</div>}
-  //     </div>
-  //   </>
-  // )
-  const handleLoc = (lat,lon) => {
+   const [data, setData] = useState();
+   //  useEffect(() => {
+   //     fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=ee22166bcb5ed367827f2147e80762a5')
+   //        .then((response) => response.json())
+   //        .then((data) => {
+   //           console.log(data);
+   //           setData(data);
+   //        })
+   //        .catch((err) => {
+   //           console.log(err.message);
+   //        });
+   //  }, []);
+   // return (
+   //   <>
+   //     <div>
+   //     {data ? <div>{JSON.stringify(data)}</div> : <div>Loading...</div>}
+   //     </div>
+   //   </>
+   // )
+   const handleLoc = (lat, lon) => {
       fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=2&appid=ee22166bcb5ed367827f2147e80762a5`)
          .then((response) => response.json())
          .then((data) => {
@@ -31,11 +31,11 @@ function App() {
          .catch((err) => {
             console.log(err.message);
          });
-   console.log(lat+" "+lon);
-  }
-return(
-  <GeoCoding handler={handleLoc}/>
-)
+      console.log(lat + " " + lon);
+   }
+   return (
+      <GeoCoding handler={handleLoc} />
+   )
 
 }
 
