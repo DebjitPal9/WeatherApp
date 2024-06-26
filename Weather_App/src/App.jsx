ba55c9e422689077377034e416043798a91fc94d
Diptoprovo <mondaldipto1606@gmail.com>
2024-06-26 12:22:05 +0530

    Added location getter

--- Weather_App/src/App.jsx
+++ Weather_App/src/App.jsx
@@ -1,25 +1,31 @@
 import React,{ useState, useEffect} from 'react'
 import Manager from './Components/Manager'
+import GeoCoding from './Components/GeoCoding'
 function App() {
-  const [data, setData] = useState();
-   useEffect(() => {
-      fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=ee22166bcb5ed367827f2147e80762a5')
-         .then((response) => response.json())
-         .then((data) => {
-            console.log(data);
-            setData(data);
-         })
-         .catch((err) => {
-            console.log(err.message);
-         });
-   }, []);
-  return (
-    <>
-      <div>
-      {data ? <div>{JSON.stringify(data)}</div> : <div>Loading...</div>}
-      </div>
-    </>
-  )
+  // const [data, setData] = useState();
+  //  useEffect(() => {
+  //     fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=ee22166bcb5ed367827f2147e80762a5')
+  //        .then((response) => response.json())
+  //        .then((data) => {
+  //           console.log(data);
+  //           setData(data);
+  //        })
+  //        .catch((err) => {
+  //           console.log(err.message);
+  //        });
+  //  }, []);
+  // return (
+  //   <>
+  //     <div>
+  //     {data ? <div>{JSON.stringify(data)}</div> : <div>Loading...</div>}
+  //     </div>
+  //   </>
+  // )
+return(
+
+  <GeoCoding/>
+)
+
 }
 
 export default App
--- Weather_App/src/Components/GeoCoding.jsx
+++ Weather_App/src/Components/GeoCoding.jsx
@@ -1,23 +1,28 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-function GeoCoding(){
-    const [data, setData] = useState(null);
+function GeoCoding() {
+    const [data, setData] = useState([]);
+    const [loc, setLoc] = useState("");
 
-
-     function handleClick(){
-        fetch('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=09e876728d98d5b64f440c7f47373d6e')
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-            setData(data);
-        })
-        .catch((err) => console.log(err.message));
+    function handleClick() {
+        setLoc("");
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${loc}&limit=3&appid=09e876728d98d5b64f440c7f47373d6e`)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                setData(data);
+            })
+            .catch((err) => console.log(err.message));
+    }
+    function location(event) {
+        setLoc(event.target.value);
     }
 
-    return(
+    return (
         <div>
+            <input type="text" value={loc} onChange={location} />
             <button onClick={handleClick}>Get data</button>
-            {data ? <div>{JSON.stringify(data)}</div> : <div>Loading ...</div>}
+            {data ? data.map((loc, index) => <div key={index}> {loc.name}, {loc.state}, {loc.country}</div>) : <div>Loading ...</div>}
         </div>
     )
 }
