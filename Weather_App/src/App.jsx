ef25a1ae9ccc014fc514085c7a84278ac0fb33f2
Debjit Pal <63508896+DebjitPal9@users.noreply.github.com>
2024-06-26 10:30:56 +0530

    Project Setup

---
+++ Weather_App/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Manager from './Components/Manager'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Manager></Manager>
+      </div>
+    </>
+  )
+}
+
+export default App
